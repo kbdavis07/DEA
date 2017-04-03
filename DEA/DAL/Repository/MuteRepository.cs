593c96c62cf902b77508fb25add652d73a097f18
kbdavis07 <kbdavis07@yahoo.com>
2017-04-03 09:20:21 -0400

    Added MuteRepository no issues, so completed.

---
+++ DEA/DAL/Repository/MuteRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using DEA.DAL.EF;
+
+
+namespace DEA.DAL.Repository
+{
+    public static class MuteRepository
+    {
+
+        public static async Task AddMuteAsync(ulong UserID, ulong GuildID, TimeSpan muteLength)
+        {
+            await BaseRepository<mute>.InsertAsync(new mute()
+            {
+                userid = UserID,
+                guildid = GuildID,
+                mutelength = muteLength
+            });
+        }
+
+        public static async Task<bool> IsMutedAsync(ulong UserID, ulong GuildID)
+        {
+            return await BaseRepository<mute>.SearchFor(c => c.userid == UserID && c.guildid == GuildID).AnyAsync();
+        }
+
+        public static async Task RemoveMuteAsync(ulong UserID, ulong GuildID)
+        {
+            var muted = await BaseRepository<mute>.SearchFor(c => c.userid == UserID && c.guildid == GuildID).FirstOrDefaultAsync();
+            if (muted != null) await BaseRepository<mute>.DeleteAsync(muted);
+        }
+
+    }
+}
