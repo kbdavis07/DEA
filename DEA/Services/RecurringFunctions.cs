92c633888c944913cc306fafebc1a15924efdeed
kbdavis07 <kbdavis07@yahoo.com>
2017-04-03 11:10:43 -0400

    Saving progress made so far. Now going to bed.

--- DEA/Services/RecurringFunctions.cs
+++ DEA/Services/RecurringFunctions.cs
@@ -1,11 +1,11 @@
-﻿using Discord.WebSocket;
-using System;
-using System.Timers;
-using DEA.SQLite.Repository;
-using DEA.SQLite.Models;
+﻿using System;
 using System.Linq;
-using Discord;
 using System.Threading.Tasks;
+using System.Timers;
+using DEA.DAL.EF;
+using DEA.DAL.Repository;
+using Discord;
+using Discord.WebSocket;
 
 namespace DEA.Services
 {
@@ -32,10 +32,10 @@ namespace DEA.Services
 
         private async void OnTimedTempMultiplierReset(object source, ElapsedEventArgs e)
         {
-            foreach (User user in BaseRepository<User>.GetAll())
+            foreach (user user in BaseRepository<user>.GetAll())
                 if (user.temporarymultiplier != 1) user.temporarymultiplier = 1;
 
-            using (var db = new DbContext())
+            using (var db = new DEAContext())
             {
                 await db.SaveChangesAsync();
             }
@@ -51,13 +51,13 @@ namespace DEA.Services
 
         private async void OnTimedApplyInterest(object source, ElapsedEventArgs e)
         {
-            foreach (var gang in BaseRepository<Gang>.GetAll())
+            foreach (var gang in BaseRepository<gang>.GetAll())
             {
-                var InterestRate = 0.025f + ((gang.Wealth / 100) * .000075f);
+                var InterestRate = 0.025f + ((gang.wealth / 100) * .000075f);
                 if (InterestRate > 0.1) InterestRate = 0.1f;
-                gang.Wealth *= 1 + InterestRate;
+                gang.wealth *= 1 + InterestRate;
             }
-            using (var db = new DbContext())
+            using (var db = new DEAContext())
             {
                 await db.SaveChangesAsync();
             }
@@ -73,40 +73,40 @@ namespace DEA.Services
 
         private async void OnTimedAutoUnmute(object source, ElapsedEventArgs e)
         {
-            foreach (Mute mute in BaseRepository<Mute>.GetAll())
+            foreach (mute mute in BaseRepository<mute>.GetAll())
             {
-                if (DateTimeOffset.Now.Subtract(mute.MutedAt).TotalMilliseconds > mute.MuteLength.TotalMilliseconds)
+                if (DateTimeOffset.Now.Subtract(mute.mutedat).TotalMilliseconds > mute.mutelength.TotalMilliseconds)
                 {
-                    var guild = _client.GetGuild(mute.GuildId);
-                    if (guild != null && guild.GetUser(mute.userid) != null)
+                    var guild = _client.GetGuild((ulong)mute.guildid);
+                    if (guild != null && guild.GetUser((ulong)mute.userid) != null)
                     {
                         var guildData = await GuildRepository.FetchGuildAsync(guild.Id);
-                        var mutedRole = guild.GetRole(guildData.MutedRoleId);
-                        if (mutedRole != null && guild.GetUser(mute.userid).Roles.Any(x => x.Id == mutedRole.Id))
+                        var mutedRole = guild.GetRole((ulong)guildData.id);
+                        if (mutedRole != null && guild.GetUser((ulong)mute.userid).Roles.Any(x => x.Id == mutedRole.Id))
                         {
-                            var channel = guild.GetTextChannel(guildData.ModLogId);
+                            var channel = guild.GetTextChannel((ulong)guildData.modlogid);
                             if (channel != null && guild.CurrentUser.GuildPermissions.EmbedLinks &&
                                 (guild.CurrentUser as IGuildUser).GetPermissions(channel as SocketTextChannel).SendMessages
                                 && (guild.CurrentUser as IGuildUser).GetPermissions(channel as SocketTextChannel).EmbedLinks)
                             {
-                                await guild.GetUser(mute.userid).RemoveRoleAsync(mutedRole);
+                                await guild.GetUser((ulong)mute.userid).RemoveRoleAsync(mutedRole);
                                 var footer = new EmbedFooterBuilder()
                                 {
                                     IconUrl = "http://i.imgur.com/BQZJAqT.png",
-                                    Text = $"Case #{guildData.CaseNumber}"
+                                    Text = $"Case #{guildData.casenumber}"
                                 };
                                 var builder = new EmbedBuilder()
                                 {
                                     Color = new Color(12, 255, 129),
-                                    Description = $"**Action:** Automatic Unmute\n**User:** {guild.GetUser(mute.userid)} ({guild.GetUser(mute.userid).Id})",
+                                    Description = $"**Action:** Automatic Unmute\n**User:** {guild.GetUser((ulong)mute.userid)} ({guild.GetUser((ulong)mute.userid).Id})",
                                     Footer = footer
                                 }.WithCurrentTimestamp();
-                                await GuildRepository.ModifyAsync(x => { x.CaseNumber++; return Task.CompletedTask; }, guild.Id);
+                                await GuildRepository.ModifyAsync(x => { x.casenumber++; return Task.CompletedTask; }, guild.Id);
                                 await channel.SendMessageAsync("", embed: builder);
                             }
                         }
                     }
-                    await MuteRepository.RemoveMuteAsync(mute.userid, mute.GuildId);
+                    await MuteRepository.RemoveMuteAsync((ulong)mute.userid, (ulong)mute.guildid);
                 }
             }
         }
